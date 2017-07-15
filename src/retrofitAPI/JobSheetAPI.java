8cde2af5def208233460722eeb50a67b62d81fae
bhaumikshah <bhaumikshah@192.168.0.103>
2017-07-15 15:19:09 +0530

    jobsheet view

---
+++ src/retrofitAPI/JobSheetAPI.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package retrofitAPI;
+
+import com.google.gson.JsonObject;
+import model.PurchaseHead;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+/**
+ *
+ * @author bhaumik
+ */
+public interface JobSheetAPI {
+
+    @GET("GetJobStatus")
+    Call<JsonObject> getJobStatus();
+
+    
+
+}
